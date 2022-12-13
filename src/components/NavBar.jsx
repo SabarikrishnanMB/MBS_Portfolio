8b9f55b57a7eef94dc6b8d0ce67006a3a2169d77
SabarikrishnanMB <mbsabarikrishnan@gmail.com>
2022-12-13 18:44:19 +0530

    First commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -34,8 +34,7 @@ const NavBar = () => {
       <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
         <div>
           <h1
-            className="text-5xl ml-2 text-white hover:text-gray-400"
-            id="fonts"
+            className="text-5xl ml-2 text-white hover:text-gray-400"            
           >
             SabariKrishnan M B
           </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   theme: {
     extend: {},
     fontFamily: {
-      signature: ["Oswald"],
+      signature: ["Roboto"],
     },
   },
   plugins: [],
